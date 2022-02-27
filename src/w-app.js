fdc8a21d0c2997aaa247d8d9a58cdaaea8bc51f6
triinliis <triinu_liis@hotmail.com>
2022-02-27 22:31:15 +0200

    removed unit conversion

--- src/w-app.js
+++ src/w-app.js
@@ -147,35 +147,7 @@ function getMyPosition(event) {
 let currentLocationButton = document.querySelector("#current-location-but");
 currentLocationButton.addEventListener("click", getMyPosition);
 
-//Fahrenheit-Celsius converter
-
-function displayFahrenheitTemperature(event) {
-  event.preventDefault();
-  let temperatureElement = document.querySelector("#currenttemperature");
-
-  celsiusLink.classList.remove("active");
-  fahrenheitLink.classList.add("active");
-  let fahrenheiTemperature = (celsiusTemperature * 9) / 5 + 32;
-  temperatureElement.innerHTML = Math.round(fahrenheiTemperature);
-}
-
-function displayCelsiusTemperature(event) {
-  event.preventDefault();
-  celsiusLink.classList.add("active");
-  fahrenheitLink.classList.remove("active");
-  let temperatureElement = document.querySelector("#currenttemperature");
-  temperatureElement.innerHTML = Math.round(celsiusTemperature);
-}
-
-let celsiusTemperature = null;
-
 let form = document.querySelector("#search-form");
 form.addEventListener("submit", handleSubmit);
-
-let fahrenheitLink = document.querySelector("#fahrenheit-link");
-fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
-
-let celsiusLink = document.querySelector("#celsius-link");
-celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
 search("Tallinn");
