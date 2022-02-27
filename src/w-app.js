9c722aefdac499febf5bffe2ee50bb0c4c9b1e6c
triinliis <triinu_liis@hotmail.com>
2022-02-27 20:41:24 +0200

    Working forecast JS

--- src/w-app.js
+++ src/w-app.js
@@ -40,35 +40,56 @@ if (minutes < 10) {
 }
 currentime.innerHTML = `${weekDay}, ${month} ${day} ⏱ ${hours}:${minutes}`;
 
-//Show forecast
-function displayForecast() {
+//Forecast days from timestamp to day name
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thur", "Fri", "Sat"];
+
+  return days[day];
+}
+
+//Forecast
+function displayForecast(response) {
+  let forcast = response.data.daily;
   let forecastElement = document.querySelector("#forecast");
-
-  let days = ["Thu", "Fri", "Sat", "Sun"];
-
   let forecastHTML = `<div class="row">`;
-  days.forEach(function (day) {
-    forecastHTML =
-      forecastHTML +
-      `
-      <div class="col-2">
-        <div class="weather-forecast-date">${day}</div>
-        <img
-          src="http://openweathermap.org/img/wn/50d@2x.png"
-          alt=""
-          width="42"
-        />
-        <div class="weather-forecast-temperatures">
-          <span class="weather-forecast-temperature-max"> 18° </span>
-          <span class="weather-forecast-temperature-min"> 12° </span>
-        </div>
-      </div>
-  `;
-  });
 
-  forecastHTML = forecastHTML + `</div>`;
+  forcast.forEach(function (forecastDay, index) {
+    if (index < 6) {
+      forecastHTML =
+        forecastHTML +
+        `<div class="col-2 text-center">
+          <div class="weather-forecast-date">${formatDay(forecastDay.dt)}</div> 
+          <img
+                  src="http://openweathermap.org/img/wn/${
+                    forecastDay.weather[0].icon
+                  }@2x.png"
+                  alt=""
+                  width="42"
+                />
+          <div class="weather-forecast-temp">
+            <span class="weather-forecast-temp-max">${Math.round(
+              forecastDay.temp.max
+            )}°C |</span>
+            <span class="weather-forecast-temp-min">${Math.round(
+              forecastDay.temp.min
+            )}°C </span> 
+          </div>
+      </div>`;
+    }
+  });
+  forecastHTML = forecastHTML + ` </div>`;
   forecastElement.innerHTML = forecastHTML;
-  console.log(forecastHTML);
+}
+
+//Forecast API
+function getForecast(coordinates) {
+  console.log(coordinates);
+  let apiKey = "737316554cb62c5b011f4119bc968d2d";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  console.log(apiUrl);
+  axios.get(apiUrl).then(displayForecast);
 }
 
 //Shows current weather
@@ -92,6 +113,8 @@ function displayTemperature(response) {
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
+
+  getForecast(response.data.coord);
 }
 
 //Search engine
